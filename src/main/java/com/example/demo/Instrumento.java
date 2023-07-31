c43a66fe3f9da5b5ac8194be53cbe958571102a1
Piero-Rodriguez-Cansaya <134323367+Piero-Rodriguez-Cansaya@users.noreply.github.com>
2023-07-31 18:07:59 +0000

    Actu3

--- src/main/java/com/example/demo/Instrumento.java
+++ src/main/java/com/example/demo/Instrumento.java
@@ -36,13 +36,13 @@ public class Instrumento {
 			Objects.equals(descripcion, instrumento.descripcion);
 	}
 
+	
+
 	@Override
 	public int hashCode() {
 
 		return Objects.hash(id, nombre, categoria, descripcion);
 	}
-
-	
 
 	@Override
 	public String toString() {
@@ -54,36 +54,35 @@ public class Instrumento {
 			'}';
 	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getCategoria() {
-        return categoria;
-    }
+	public String getCategoria() {
+		return categoria;
+	}
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
 
-    public String getDescripcion() {
-        return descripcion;
-    }
+	public String getDescripcion() {
+		return descripcion;
+	}
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 }
